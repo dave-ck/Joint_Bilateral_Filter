caba140cd7c7eefcec3e662a6201a487d3f9b837
dave-ck <daveck@protonmail.com>
2018-12-11 19:24:19 +0000

    working bilateral filter with color

--- bilateral_filter.py
+++ bilateral_filter.py
@@ -1,5 +1,4 @@
-import time
-
+from time import strftime
 import cv2
 import numpy as np
 import math
@@ -10,57 +9,90 @@ def neighborhood(coordinates, radius, xmax, ymax):
     x, y = coordinates
     for i in range(-1 * radius, radius + 1):
         for j in range(-1 * radius, radius + 1):
-            n_x = -1*abs(abs(x+i)-xmax)+xmax
-            n_y = -1*abs(abs(y+j)-ymax)+ymax
+            n_x = -1 * abs(abs(x + i) - xmax) + xmax
+            n_y = -1 * abs(abs(y + j) - ymax) + ymax
             neighbors.append((n_x, n_y))
     return neighbors
 
 
 def g(x, sigma):
     # implement bottom-up dynamic programming
-    return np.exp(-(x**2) / (2 * sigma**2))
+    return np.exp(-(x ** 2) / (2 * sigma ** 2))
 
 
 def bilateral_filter(input_image, radius, sigma_intensity, sigma_space):
     # detect if input is 1-channel or 3-channel
     output_image = input_image.copy()
     lab_image = cv2.cvtColor(input_image, cv2.COLOR_BGR2LAB)
-    xmax, ymax = len(input_image), len(input_image[0])
-    for x in range(len(input_image)):
-        for y in range(len(input_image[1])):
-            output_image[x, y] = pixel_response(input_image, (x, y), radius, sigma_intensity, sigma_space, xmax, ymax)
+    x_max, y_max = len(input_image), len(input_image[0])
+    for x in range(x_max):
+        for y in range(y_max):
+            # compute pixel response
+            numerator = 0
+            divisor = 0
+            # summation over the neighborhood of the input pixel
+            for n_coordinates in neighborhood((x, y), radius, x_max - 1, y_max - 1):
+                # compute the numerator
+                space_term = g(distance((x, y), n_coordinates), sigma_space)
+                # use euclidean distance in CIE-LAB space, as paper suggested
+                intensity_term = g(np.linalg.norm(cv2.absdiff(lab_image[(x, y)], lab_image[n_coordinates])),
+                                   sigma_intensity)
+                divisor_add = space_term * intensity_term
+                numerator += divisor_add * input_image[n_coordinates]
+                # compute the divisor
+                divisor += divisor_add
+            output_image[x, y] = numerator / divisor
+    return output_image
+
+
+def joint_bilateral_filter(flash_image, noflash_image, radius, sigma_intensity, sigma_space):
+    # detect if input is 1-channel or 3-channel
+    output_image = flash_image.copy()
+    lab_image = cv2.cvtColor(flash_image, cv2.COLOR_BGR2LAB)
+    x_max, y_max = len(flash_image), len(flash_image[0])
+    for x in range(x_max):
+        for y in range(y_max):
+            # compute pixel response
+            numerator = 0
+            divisor = 0
+            # summation over the neighborhood of the input pixel
+            for n_coordinates in neighborhood((x, y), radius, x_max - 1, y_max - 1):
+                # compute the numerator
+                space_term = g(distance((x, y), n_coordinates), sigma_space)
+                # use euclidean distance in CIE-LAB space, as paper suggested
+                intensity_term = g(np.linalg.norm(cv2.absdiff(lab_image[(x, y)], lab_image[n_coordinates])),
+                                   sigma_intensity)
+                divisor_add = space_term * intensity_term
+                numerator += divisor_add * noflash_image[n_coordinates]
+                # compute the divisor
+                divisor += divisor_add
+            output_image[x, y] = numerator / divisor
     return output_image
 
 
 def distance(coordinates1, coordinates2):
-    return math.hypot(coordinates1[0] - coordinates2[0], coordinates1[1] - coordinates2[1])
+    # wrap in np.abs to ensure np array is input for np.linalg.norm
+    coordinates1, coordinates2 = np.abs(coordinates1), np.abs(coordinates2)
+    return np.linalg.norm(coordinates1-coordinates2)
 
 
-def pixel_response(input_image, p_coordinates, radius, sigma_intensity, sigma_space, xmax, ymax):
-    numerator = 0
-    divisor = 0
-    # summation over the neighborhood of the input pixel
-    for n_coordinates in neighborhood(p_coordinates, radius, xmax-1, ymax-1):
-        # compute the numerator
-        space_term = g(distance(p_coordinates, n_coordinates), sigma_space)
-        intensity_term = g(cv2.absdiff(int(input_image[p_coordinates]), int(input_image[n_coordinates]))[0], sigma_intensity)
-        numerator += space_term * intensity_term * input_image[n_coordinates]
-        # compute the divisor
-        divisor += space_term * intensity_term
-    response = numerator / divisor
-    return response
+def write_image(img):
+    path = "outputs/"
+    path += strftime("%d %b %Y %H:%M:%S")
 
 
 def main():
-    img = cv2.imread("test1.png")
+    img = cv2.imread("test2.png")
     gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     # timing_test(gray_img, 1, 1, 1)
-    result_cv2 = cv2.bilateralFilter(gray_img, 5, 20, 20)
-    result = bilateral_filter(gray_img, 2, 20, 20)
+    result_cv2 = cv2.bilateralFilter(img, 5, 20, 20)
+    result = bilateral_filter(img, 2, 20, 20)
     cv2.imshow("Homebrew Bilateral, ", result)
     cv2.imshow("CV2 Bilateral", result_cv2)
     cv2.imshow("Originalpicc", img)
+
     cv2.waitKey()
 
-flags = [i for i in dir(cv2) if i.startswith('COLOR_BGR2') and "lab" in i.lower()]
-print(flags)
+
+
+main()
